dea87b5349f4325cc54b5461ca972f7aa3c24620
adamvnovak <adamvnovak@protonmail.com>
2023-03-17 14:55:42 -0700

    alexandria

---
+++ alexandria.js
@@ -0,0 +1,41 @@
+document.getElementById("searchButton").addEventListener("click", searchBooks);
+
+// async function searchBooks() {
+//   const query = document.getElementById("search").value;
+//   const url = `https://libgen-api.herokuapp.com/search/${query}`;
+//   const response = await fetch(url);
+//   const data = await response.json();
+//   displayResults(data);
+// }
+
+// function displayResults(books) {
+//   const results = document.getElementById("results");
+//   results.innerHTML = "";
+//   books.forEach(book => {
+//     const bookDiv = document.createElement("div");
+//     bookDiv.classList.add("book");
+//     bookDiv.innerHTML = `<h3>${book.title}</h3><p>Author: ${book.author}</p>`;
+//     results.appendChild(bookDiv);
+//   });
+// }
+
+async function searchBooks(query) {
+  const url = `https://libgen.lc/json.php?fields=Title,Author,Download&search=${encodeURIComponent(query)}`;
+  const response = await fetch(url);
+  const data = await response.json();
+  displayResults(data);
+}
+
+function displayResults(books) {
+  const results = document.getElementById("results");
+  results.innerHTML = "";
+  books.forEach(book => {
+    const bookDiv = document.createElement("div");
+    bookDiv.classList.add("book");
+    bookDiv.innerHTML = `<h3>${book.Title}</h3><p>Author: ${book.Author}</p>`;
+    results.appendChild(bookDiv);
+  });
+}
+
+// Example usage:
+searchBooks("JavaScript");
